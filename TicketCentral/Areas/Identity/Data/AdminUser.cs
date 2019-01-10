6506c708e53847d453e7753be46eb58a5c082b12
Charlene Badibanga <21800597@bucks.ac.uk>
2019-01-10 22:11:37 +0000

    added identity scaffolding for admin

---
+++ TicketCentral/Areas/Identity/Data/AdminUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TicketCentral.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AdminUser class
+    public class AdminUser : IdentityUser
+    {
+        [PersonalData]
+        public String Name { get; set; }
+    }
+}
